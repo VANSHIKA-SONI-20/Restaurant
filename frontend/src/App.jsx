15cd9dc4ee3691b6e14e999609490db42a56991b
VANSHIKA-SONI-20 <sonivanshika630@gmail.com>
2025-07-03 18:00:22 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import Home from './Pages/Home/Home';
-import NotFound from './Pages/NotFound/NotFound';
-import Success from './Pages/Success/Success';
+import Home from '../Pages/Home/Home';
+import NotFound from '../Pages/NotFound/NotFound';
+import Success from '../Pages/Success/Success'
+
 import './App.css'
 const App = () => {
   return (
